1b399f6c235ef818a4f7633b77483f62e99b40a3
javert185 <124713607+javert185@users.noreply.github.com>
2023-03-26 18:27:42 +0200

    Add files via upload

---
+++ s1t2n1exercici1/Producte.java
@@ -0,0 +1,21 @@
+package s1t2n1exercici1;
+
+public class Producte {
+	
+	private String nom;
+	private int preu;
+	
+	public Producte(String nom, int preu) {
+		this.nom = nom;
+		this.preu = preu;
+				
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	
+	public int getPreu() {
+		return preu;
+	}
+}
