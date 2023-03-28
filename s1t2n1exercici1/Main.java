b4f25fb9413c1ad01d6b2d9e77f918c8eb150ce5
javert185 <124713607+javert185@users.noreply.github.com>
2023-03-28 14:09:10 +0200

    Add files via upload

---
+++ s1t2n1exercici1/Main.java
@@ -0,0 +1,37 @@
+package s1t2n1exercici1;
+
+import javax.swing.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//Ficar productes
+		
+		String nou_producte;	
+		do {
+			
+			//Preguntem per un producte
+			nou_producte = JOptionPane.showInputDialog("Afegeix un producte");
+
+			//Si l'usuari no fica producte, ni preguntem pel preu
+			if (!nou_producte.equals("")) {
+				
+			//Preguntem pel preu i el convertim a int
+			int preu_nou_producte = Integer.parseInt(JOptionPane.showInputDialog("Introdueix el seu preu:" ));
+				
+				//Afegim el producte a la llista producte
+				Venda.afegir_producte(nou_producte, preu_nou_producte);
+			}		
+		} while (!nou_producte.equals(""));
+		
+		Venda.calcularTotal();
+		
+		//Codi necessari per a generar i capturar una excepció de tipus index out of bounds exception
+		try {
+				System.out.println(Venda.getColleccio().get(Venda.getColleccio().size() + 1));
+		    } catch (Exception e) {
+		      System.out.println("Excepció IndexOutOfBoundsException");
+		    }
+	}
+}
---
+++ s1t2n1exercici1/VendaBuidaException.java
@@ -0,0 +1,9 @@
+package s1t2n1exercici1;
+
+class VendaBuidaException extends Exception {
+	
+	public VendaBuidaException(String message) {
+		
+		super(message);	
+	}
+}
